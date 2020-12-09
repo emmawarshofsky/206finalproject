4b8f7a594458a99e33cca58ae99e8a9687fa417a
efinkel <efinkel@umich.edu>
2020-12-09 14:18:18 -0500

    day 2 added some data to db, problems in comments

--- 206final.py
+++ 206final.py
@@ -6,6 +6,7 @@ import json
 import tweepy
 from bs4 import BeautifulSoup
 import twitter_info
+from datetime import datetime
 
 consumer_key_f = twitter_info.consumer_key_f
 consumer_key_secret_f = twitter_info.consumer_key_secret_f
@@ -16,8 +17,23 @@ def save_to_database(tw_text, cur, conn):
     cur.execute("DROP TABLE IF EXISTS Tweets")
     cur.execute("CREATE TABLE Tweets (tweet_id INTEGER KEY, tweet_text TEXT, tweet_date TEXT, month_id INTEGER)")
     for i in range(len(tw_text)):
-        cur.execute("INSERT INTO Tweets (id, tweet_text, tweet_date, month_id) VALUE (?)", (i, ))
-    pass
+        timestamp = tw_text[i][1]
+        date = timestamp.strftime("%m/%d/%Y")
+        month_id = timestamp.strftime("%m")
+        tweet_text = parse_tweet(tw_text[i][0]) 
+        cur.execute('''INSERT INTO Tweets (tweet_id, tweet_text, tweet_date, month_id) 
+            VALUES (?, ?, ?, ?)''', (i, tweet_text, date, month_id)) #db doesn't look like it's getting full tweet...?
+        #need to figure out what is "duplicate", retweets don't add a lot. 
+    conn.commit()
+
+#function to parse through tweet text and get rid of unnecessary info
+def parse_tweet(tweet_text):
+    tweet_text = tweet_text.strip().lower() 
+    words = tweet_text.split()
+    if words[-1].startswith("https://"):
+        words = words[:-1]
+    tweet = " ".join(words)
+    return tweet
 
 auth = tweepy.OAuthHandler(consumer_key_f, consumer_key_secret_f)
 auth.set_access_token(access_token_f, access_token_secret_f)
