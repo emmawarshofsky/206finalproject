a4f3196efce78bb4b95d7e4138135b0d11043cbf
efinkel <efinkel@umich.edu>
2020-12-10 20:45:17 -0500

    trying to import nltk for analysis

--- 206final.py
+++ 206final.py
@@ -1,4 +1,5 @@
 #finalproject
+import sys
 import requests
 import sqlite3
 import os
@@ -8,6 +9,10 @@ from bs4 import BeautifulSoup
 import twitter_info
 from datetime import datetime
 import string
+import nltk
+from nltk.sentiment import SentimentAnalyzer
+from nltk.sentiment import SentimentIntensityAnalyzer
+
 
 consumer_key_f = twitter_info.consumer_key_f
 consumer_key_secret_f = twitter_info.consumer_key_secret_f
@@ -32,13 +37,18 @@ def save_to_database(tw_text, cur, conn):
 def parse_tweet(tweet_text):
     tweet_text = tweet_text.strip().lower() 
     words = tweet_text.split()
-    if words[-1].startswith("https://"):
-        words = words[:-1]
+    idx = None
+    for i in range(len(words)):
+        if words[i].startswith("https://") or words[i].startswith("http://"):
+            idx = i
+    if idx != None:
+        words.remove(words[idx])
     tweet = " ".join(words)
     return tweet
 
 
 #counts the occurrences for each words, returns a dictionary
+#come back and see if we need this
 def count_words(cur, conn):
     full_path = os.path.join(os.path.dirname(__file__), "stopwords.txt")
     f = open(full_path, "r")
@@ -54,16 +64,27 @@ def count_words(cur, conn):
         tweets = cur.fetchall()
         for tweet in tweets:
             tweet = tweet[0]
-            tweet = tweet.strip(string.punctuation)
+            tweet = tweet.translate(str.maketrans('', '', string.punctuation))
+            tweet = tweet.translate(str.maketrans('', '', '1234567890'))
             words = tweet.split()
             for word in words:
                 if word not in stop_words:
                     word_counts[word] = word_counts.get(word, 0) + 1
+        word_counts = dict(sorted(word_counts.items(), key = lambda x: x[1], reverse = True))
         return word_counts
     except:
         print("Error: couldn't retrieve tweet texts")
         return {}
+    
+    def tweet_analysis(cur, conn):
+        cur.execute("SELECT * FROM Tweets")
+        text_tpls = cur.fetchall()[0]
+        sid = SentimentIntensityAnalyzer()
+        for text in text_tpls:
+            score = sid.polarity_scores(text)
+            print(score)
 
+        pass
 
 auth = tweepy.OAuthHandler(consumer_key_f, consumer_key_secret_f)
 auth.set_access_token(access_token_f, access_token_secret_f)
@@ -84,7 +105,8 @@ for tweet in tweets:
     tw_text.append(text_date)
 save_to_database(tw_text, cur, conn)
 word_counts_dict = count_words(cur, conn)
-print(word_counts_dict) #going to have to add our own stop words
+#print(word_counts_dict) #going to have to add our own stop words
 
+tweet_analysis(cur, conn)
 
 conn.close()
