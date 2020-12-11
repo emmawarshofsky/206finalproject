2cb4f4e70208b6b528f9bdb85f49a8148b3a70ed
efinkel <efinkel@umich.edu>
2020-12-11 15:07:40 -0500

    started writing stuff to csv file

--- 206final.py
+++ 206final.py
@@ -5,6 +5,7 @@ import sqlite3
 import os
 import json
 import tweepy
+import csv
 from bs4 import BeautifulSoup
 import twitter_info
 from datetime import datetime
@@ -50,7 +51,7 @@ def parse_tweet(tweet_text):
 
 
 #counts the occurrences for each words, returns a dictionary
-#come back and see if we need this
+#DELETE THIS FUNCTION?
 def count_words(cur, conn):
     full_path = os.path.join(os.path.dirname(__file__), "stopwords.txt")
     f = open(full_path, "r")
@@ -79,28 +80,54 @@ def count_words(cur, conn):
         return {}
 
 def tweet_analysis(cur, conn):
-    i = 1
+    i = 2
     overall_scores = {} #dict of dicts, keys are dates, values dict with the sum
-    while i <= 8:
+    while i <= 9:
         date = "12/0{}/2020".format(str(i))
         cur.execute("SELECT tweet_text FROM Tweets WHERE date = ?", (date, ))
-        text_tpls = cur.fetchall()
-        pos_sum = 0.0
-        neg_sum = 0.0
-        num_tweets = 0
-        for tweet in text_tpls:
-            sid = SentimentIntensityAnalyzer()
-            analysis = sid.polarity_scores(tweet[0])
-            num_tweets += 1
-            pos_sum += analysis["pos"] * 100
-            neg_sum += analysis["neg"] * 100
-        pos_avg = float(pos_sum / num_tweets)
-        neg_avg = float(neg_sum / num_tweets)
-        day_dict = {"positive average": pos_avg, "negative average": neg_avg}
-        overall_scores[date] = day_dict
-    print(overall_scores)
-    #if this works could get rid of word counts
-    pass
+        try:
+            text_tpls = cur.fetchall()
+            pos_sum = 0.0
+            neg_sum = 0.0
+            neutral_sum = 0.0
+            compound_sum = 0.0
+            num_tweets = 0
+            for tweet in text_tpls:
+                sid = SentimentIntensityAnalyzer()
+                analysis = sid.polarity_scores(tweet[0])
+                num_tweets += 1
+                pos_sum += analysis["pos"] 
+                neg_sum += analysis["neg"]
+                neutral_sum += analysis["neu"]
+                compound_sum += analysis["compound"]
+            pos_avg = round(float(pos_sum / num_tweets), 4)
+            neg_avg = round(float(neg_sum / num_tweets), 4)
+            neutral_avg = round(float(neutral_sum / num_tweets), 4)
+            average = round(float(compound_sum / num_tweets), 4)
+            day_dict = {"positive average": pos_avg, "negative average": neg_avg, 
+                "neutral average": neutral_avg, "overall average": average}
+            overall_scores[date] = day_dict
+            i += 1
+        except:
+            print("Error")
+            i += 1
+            return {}
+    return overall_scores
+
+def write_to_csv(twitter_data, filename):
+    with open(filename, "w") as outfile:
+        csv_writer = csv.writer(outfile, delimiter = ",")
+        headers = ["Date", "Average Positive Score", "Average Negative Score", "Average Neutral Score", "Overall Average"]
+        csv_writer.writerow(headers)
+        for day in list(twitter_data.items()):
+            row = []
+            row.append(day[0])
+            for score in list(day[1].items()):
+                row.append(score[1])
+            csv_writer.writerow(row)
+
+
+
 
 auth = tweepy.OAuthHandler(consumer_key_f, consumer_key_secret_f)
 auth.set_access_token(access_token_f, access_token_secret_f)
@@ -113,16 +140,17 @@ cur = conn.cursor()
 
 hashtag = "#michiganfootball"
 start_date = "2020-09-26"
-end_date = "2020-12-09" #this date gives more items
+end_date = "2020-12-10" #this date gives more items
 tweets = tweepy.Cursor(api.search, q = hashtag, lang = "en", since = start_date, until = end_date, tweet_mode = "extended").items(500)
 tw_text = []
 for tweet in tweets:
     text_date = (tweet.full_text, tweet.created_at)
     tw_text.append(text_date)
 save_to_database(tw_text, cur, conn)
-word_counts_dict = count_words(cur, conn)
+#word_counts_dict = count_words(cur, conn)
 #print(word_counts_dict) #going to have to add our own stop words
 
-tweet_analysis(cur, conn)
+twitter_data = tweet_analysis(cur, conn)
+write_to_csv(twitter_data, "twitter_sentiments.csv")
 
 conn.close()
---
+++ facebook.py
@@ -0,0 +1,7 @@
+#facebook
+import requests
+import json
+import facebook
+import os
+import sqlite3
+import facebook_info
